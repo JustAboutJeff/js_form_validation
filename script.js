699386efdd915bc32ca005e8b284ebb778a69257
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-13 08:59:11 -0500

    Adds in regex matching and error reporting on each form submission

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
 $(document).ready( function() {
-    $('button').on('click', function() {
-      validateForm();
-    });
+    
+  $('button').on('click', function( event ) {
+    event.preventDefault();
+    validateForm();
+  });
+
+  function validateForm() {
+    var emailRegex = /\S+@\S+\.\S+/;
+    var passNumericRegex = /.*\d.*/;
+    var passCapitalRegex = /.*[A-Z]+.*/;
+
+    var $errorList = $('ul#errors');
+    $errorList.contents().remove();
+
+    var $formInput = $('form').filter(':input');
+    var $emailInput = $('input[name="email"]').val();
+    var $passInput = $('input[name="password"]').val();
+    
+    if (!emailRegex.test($emailInput)) {
+      $errorList.append('<li>Must be a valid email address</li>');
+    }
+    if (!passNumericRegex.test($passInput)) {
+      $errorList.append('<li>Password must have at least one numeric character (0-9)</li>');
+    }
+    if (!passCapitalRegex.test($passInput)) {
+      $errorList.append('<li>Password must have at least one capital letter</li>');
+    }
+    if ($passInput.length < 8 ) {
+      $errorList.append('<li>Password must be at least 8 characters long</li>');
+    }
+  }
 });
-
-function validateForm() {
-  var $errorList = $('ul#errors');
-
-  var emailRegex = /\S+@\S+\.\S+/;
-  var $emailInput = $('input[name=email]').val();
-  var passRegex = /..../;
-  var $passInput = $('input[name=password]').val();
-
-  if !(regex.exec() {
-            $errorList.append('<li></li>').html('Must be a valid email address');
-        }
-}
